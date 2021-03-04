9bd0a47f005b50c010a9339a5bd1a65c39785d6c
emcog <robin.leonard@gmail.com>
2021-03-04 17:40:08 +0000

    updates

--- js/main.js
+++ js/main.js
@@ -274,8 +274,8 @@ var $$ = {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/robin/Sites/skeleventy/resources/js/main.js */"./resources/js/main.js");
-module.exports = __webpack_require__(/*! /Users/robin/Sites/skeleventy/resources/sass/main.scss */"./resources/sass/main.scss");
+__webpack_require__(/*! /Users/robin/Sites/lomondhillsorganics/resources/js/main.js */"./resources/js/main.js");
+module.exports = __webpack_require__(/*! /Users/robin/Sites/lomondhillsorganics/resources/sass/main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
